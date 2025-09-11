e7257668e41b6b86f353c637717f63888c5883e0
NatalieCheong <50092072+NatalieCheong@users.noreply.github.com>
2025-09-11 08:46:51 +0800

    Delete studio_config.py

--- studio_config.py
+++
@@ -1,8 +0,0 @@
-from langgraph_financial_agent import LangGraphFinancialAgent
-
-# Export your graph for Studio
-agent = LangGraphFinancialAgent()
-graph = agent.graph
-
-# This makes it available to Studio
-__all__ = ["graph"]
